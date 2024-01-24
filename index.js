a19ded0628b8bcc625c74b33da90971f40ae5e5d
Selvin <98340611+DinteN098@users.noreply.github.com>
2024-01-23 23:25:07 -0500

    Update index.js
    added a test case handler that makes sure that user uses a numerical value on the electric usage box

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
-//event listener when the add button is pressed
-$("#addButton").click(function(){
-    //avoid refreshing page if user presses add button
+$("#addButton").click(function(event){
+    // Avoid refreshing page if the user presses the add button
     event.preventDefault();
 
-    //value for name, electric usage, and electric measurement
+    // Value for name, electric usage, and electric measurement
     var itemName = $("#elecName").val();
     var itemUsage = $("#elecUsage").val();
-    var ElectricMeasurement = $("#measurements").val();
+    var electricMeasurement = $("#measurements").val();
 
-    //making sure we get user's input
-    if (itemName == "" || itemUsage == ""){
-        alert("You need a name and its electric usage.")
+    // Validate if itemUsage is a valid integer
+    if (!Number.isInteger(Number(itemUsage))) {
+        alert("Electric usage must be an integer.");
+    } else {
+        // Making sure we get the user's input
+        if (itemName === "" || itemUsage === "") {
+            alert("You need a name and its electric usage.");
+        } else {
+            // Adding all the above values into a string
+            var addItem = $("<li>").text(`${itemName} --${itemUsage} ${electricMeasurement}`);
+            
+            // Add the string from above into the unordered list
+            $("#listOfItems").append(addItem);
+
+            // Clearing the text boxes
+            $("#elecName").val('');
+            $("#elecUsage").val('');
+        }
     }
-    else{
-    //adding all the above values into a string
-    var addItem = $("<li>").text(`${itemName} --${itemUsage} ${ElectricMeasurement}`);
-    
-    //add the string from above into the unlisted list
-    $("#listOfItems").append(addItem);
-
-    //clearing the text boxes
-    $("#elecName").val('');
-    $("#elecUsage").val('');
-    }
-
-
-})
+});
